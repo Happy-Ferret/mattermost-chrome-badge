9e673194b9b9ce294fb83b02d85a16879fecda22
dahdahm <mich.dahdah@gmail.com>
2016-03-11 13:19:20 -0500

    Bade color not changes to: * Red when the chat tab is not open * Blue when a group or channel message is unread.

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,35 +1,71 @@
-var chat = "192.168.47.60";
- function doStuff() {
-	checkMessages()
-	setTimeout(doStuff, 2000);
-}
-setTimeout(doStuff, 2000);
+var chat = "http://192.168.47.60:8065";
+var pingDuration = 2000;
+var privateMessageBadge = "icons/icon16.png";
+var closedTabBadge = "icons/iconred16.png";
+var groupOrChannelBadge = "icons/iconblue36.png";
 
-function checkMessages(){
-var found = false;
-chrome.tabs.getAllInWindow(null, function(tabs){
+chrome.browserAction.onClicked.addListener(function(icon) {
+  goToChat()
+});
+/*chrome.tabs.update(tabId, {selected: true});*/
+function getChatTab() {
+
+}
+
+function goToChat() {
+  chrome.tabs.create({
+    url: chat
+  });
+}
+
+
+
+function ping() {
+  checkMessages()
+  setTimeout(ping, pingDuration);
+}
+setTimeout(ping, pingDuration);
+
+function checkMessages() {
+  var tabFound = false;
+  chrome.tabs.getAllInWindow(null, function(tabs) {
     for (var i = 0; i < tabs.length; i++) {
-	if(tabs[i].url.indexOf(chat) > -1 && tabs[i].title.startsWith("(")){
-		found=true;
-		var title = tabs[i].title;
-		chrome.browserAction.setBadgeText({text: title});
-		var rgx = /\(([^)]+)\)/;
-		var matcher = title.match(rgx);
-		var notif = matcher && matcher[1];
-		chrome.browserAction.setBadgeText({text: notif});
-	}
+      if (tabs[i].url.indexOf(chat) > -1) {
+        tabFound = true;
+        chrome.browserAction.setIcon({
+          path: privateMessageBadge
+        });
+        var title = tabs[i].title;
+        if (title.indexOf("*") > -1) {
+          chrome.browserAction.setIcon({
+            path: groupOrChannelBadge
+          });
+          chrome.browserAction.setBadgeText({
+            text: ""
+          });
+        }
+        if (title.indexOf("(") > -1) {
+          chrome.browserAction.setBadgeText({
+            text: title
+          });
+          var rgx = /\(([^)]+)\)/;
+          var matcher = title.match(rgx);
+          var privateMsgCount = matcher && matcher[1];
+          chrome.browserAction.setBadgeText({
+            text: privateMsgCount
+          });
+        }
+      }
     }
 
-    if(!found)
-	chrome.browserAction.setBadgeText({text: ""});
-});
+    if (!tabFound) {
+      chrome.browserAction.setBadgeText({
+        text: ""
+      });
+      chrome.browserAction.setIcon({
+        path: closedTabBadge
+      });
+    }
+  });
 
 }
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-//  	chrome.pageAction.show(sender.tab.id);
-//    sendResponse();
-chrome.browserAction.setBadgeText({text: "10+"});
-alert("hello");
-  });
